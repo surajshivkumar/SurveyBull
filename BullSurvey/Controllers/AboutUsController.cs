78d76dcca3df16ceae33d6dccd4843ebbd0b6496
SharvMore <sharvarimore90@gmail.com>
2024-11-09 17:20:12 -0500

    Added the AboutUs Page and some styling

---
+++ BullSurvey/Controllers/AboutUsController.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+using BullSurvey.Models;
+
+namespace BullSurvey.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public IActionResult AboutUs()
+        {
+            var viewModel = new AboutUsViewModel
+            {
+                WebsiteName = "SurveyBull",
+                Description = "SurveyBull is a leading platform for creating and managing surveys.",
+                TeamMembers = new List<TeamMember>
+                {
+                    new TeamMember
+                    {
+                        Name = "Suraj Shiva Kumar",
+                        PhotoUrl = "/images/suraj.jpg",
+                        Role = "BAIS Student",
+                        Bio = "Suraj is a passionate leader with over 3 years of experience in backend development."
+                    },
+                    new TeamMember
+                    {
+                        Name = "Shivani Suryawanshi",
+                        PhotoUrl = "/images/shivani.jpeg",
+                        Role = "BAIS Student",
+                        Bio = "Shivani is an expert in technology solutions, with a background in software engineering."
+                    },
+                    new TeamMember
+                    {
+                        Name = "Sharvari More",
+                        PhotoUrl = "/images/Sharvari.jpg",
+                        Role = "BAIS Student",
+                        Bio = "Sharvari has 3 years of experience in frontend development and customer engagement."
+                    },
+                     new TeamMember
+                    {
+                        Name = "Venkata Sai Satvik Reddy Tanuboddi",
+                        PhotoUrl = "/images/satvik.jpg",
+                        Role = "BAIS Student",
+                        Bio = "Satvik has a decade of experience in business analytics and data insights."
+                    }
+                }
+            };
+
+            return View(viewModel);
+        }
+    }
+}
