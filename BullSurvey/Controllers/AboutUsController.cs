cb12294ddf85c16751959be9c1f2447801c2ce99
shiva <shiva@SHIVANI_LAPTOP>
2024-11-10 23:05:10 -0500

    Bio changes

--- BullSurvey/Controllers/AboutUsController.cs
+++ BullSurvey/Controllers/AboutUsController.cs
@@ -18,28 +18,28 @@ namespace BullSurvey.Controllers
                         Name = "Suraj Shiva Kumar",
                         PhotoUrl = "/images/suraj.jpg",
                         Role = "BAIS Student",
-                        Bio = "Suraj is a passionate leader with over 3 years of experience in backend development."
+                        Bio = "Suraj is a passionate leader with over three years of experience in backend development."
                     },
                     new TeamMember
                     {
                         Name = "Shivani Suryawanshi",
                         PhotoUrl = "/images/shivani.jpeg",
                         Role = "BAIS Student",
-                        Bio = "Shivani is an expert in technology solutions, with a background in software engineering."
+                        Bio = "Shivani Suryawanshi has over two years of experience in technology solutions as a Software Engineer."
                     },
                     new TeamMember
                     {
                         Name = "Sharvari More",
                         PhotoUrl = "/images/Sharvari.jpg",
                         Role = "BAIS Student",
-                        Bio = "Sharvari has 3 years of experience in frontend development and customer engagement."
+                        Bio = "Sharvari has three years of experience in frontend development and customer engagement."
                     },
                      new TeamMember
                     {
                         Name = "Venkata Sai Satvik Reddy Tanuboddi",
                         PhotoUrl = "/images/satvik.jpg",
                         Role = "BAIS Student",
-                        Bio = "Satvik has a decade of experience in business analytics and data insights."
+                        Bio = "Satvik is a data-driven professional with two years of experience as a Data Analyst."
                     }
                 }
             };
