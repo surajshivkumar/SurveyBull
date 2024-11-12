7c5c77feb4cf0442dd7b52d0fbb0463f5326b6b7
surajshivakumar <surajshivakumar53@gmail.com>
2024-11-12 01:14:08 -0500

    doughnut chart added

--- BullSurvey/Models/SurveyDashboardViewModel.cs
+++ BullSurvey/Models/SurveyDashboardViewModel.cs
@@ -1,30 +1,42 @@
-namespace BullSurvey.Models;
-// Models/ViewModels/SurveyDashboardViewModel.cs
-public class SurveyDashboardViewModel
+namespace BullSurvey.Models
 {
-    public Survey Survey { get; set; }
-    public int TotalResponses { get; set; }
-    public double ResponseRate { get; set; }
-    public List<QuestionAnalyticViewModel> QuestionAnalytics { get; set; }
-    public List<TimeSeriesDataPoint> TimeSeries { get; set; }
-}
+    // Main ViewModel to pass to the view
+    public class SurveyDashboardViewModel
+    {
+        public Survey Survey { get; set; }
+        public int TotalResponses { get; set; }
+        public double ResponseRate { get; set; }
+        public List<QuestionAnalyticViewModel> QuestionAnalytics { get; set; }
+        public List<TimeSeriesDataPoint> TimeSeries { get; set; }
+    }
 
-public class QuestionAnalyticViewModel
-{
-    public int QuestionId { get; set; }
-    public int Count { get; set; }
-    public List<QuestionOptionViewModel> Options { get; set; }
-}
+    // ViewModel for each question's analytics
+    public class QuestionAnalyticViewModel
+    {
+        public int QuestionId { get; set; }
+        public int Count { get; set; }
+        public List<AnswerViewModel> Answers { get; set; }  // Added this property to store answers and their counts
+    }
 
-public class QuestionOptionViewModel
-{
-    public int? OptionId { get; set; }
-    public string TextAnswer { get; set; }
-    public int Count { get; set; }
-}
+    // ViewModel for the answers, including their text and count
+    public class AnswerViewModel
+    {
+        public string TextAnswer { get; set; }  // The text of the answer
+        public int Count { get; set; }          // The number of times this answer was selected
+    }
 
-public class TimeSeriesDataPoint
-{
-    public DateTime Date { get; set; }
-    public int Count { get; set; }
+    // ViewModel for question options (if needed for displaying options)
+    public class QuestionOptionViewModel
+    {
+        public int? OptionId { get; set; }
+        public string TextAnswer { get; set; }
+        public int Count { get; set; }
+    }
+
+    // ViewModel for time series data (date and count of responses on each day)
+    public class TimeSeriesDataPoint
+    {
+        public DateTime Date { get; set; }
+        public int Count { get; set; }
+    }
 }
