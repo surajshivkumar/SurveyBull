63399fe5cc008ae70affb0abbada73dddbef9b7f
surajshivakumar <surajshivakumar53@gmail.com>
2024-11-12 16:12:12 -0500

    fix-num questions

---
+++ BullSurvey/Migrations/20241112064320_AddDashboard2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BullSurvey.Migrations
+{
+    public partial class AddDashboard2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
